58ffbba39a8ff829e20744d8ab57084e11e81ffe
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-12-02 02:28:15 +0700

    reordering

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import "../styles/tailwind.css";
 import "../styles/slick.css";
-
+import "../styles/tailwind.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
