0eabdc50550219628769570eb28880e5deb8debe
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-12-02 20:26:25 +0700

    improve mobile

--- components/Hero.js
+++ components/Hero.js
@@ -50,17 +50,19 @@ const Hero = ({
         <div className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
           {listUser.map((listUsers, index) => (
             <div
-              className="flex items-center justify-start sm:justify-center py-4 sm:py-6 w-8/12 px-8 sm:w-auto mx-auto sm:mx-0"
+              className="flex items-center justify-start sm:justify-center py-4 sm:py-6 w-8/12 px-4 sm:w-auto mx-auto sm:mx-0"
               key={index}
             >
-              <div className="flex items-center justify-center bg-orange-100 w-12 h-12 mr-6 rounded-full">
-                <img src={listUsers.icon} className="h-6 w-6" />
-              </div>
-              <div className="flex flex-col">
-                <p className="text-xl text-black-600 font-bold">
-                  {listUsers.number}+
-                </p>
-                <p className="text-lg text-black-500">{listUsers.name}</p>
+              <div className="flex mx-auto w-40 sm:w-auto">
+                <div className="flex items-center justify-center bg-orange-100 w-12 h-12 mr-6 rounded-full">
+                  <img src={listUsers.icon} className="h-6 w-6" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-xl text-black-600 font-bold">
+                    {listUsers.number}+
+                  </p>
+                  <p className="text-lg text-black-500">{listUsers.name}</p>
+                </div>
               </div>
             </div>
           ))}
