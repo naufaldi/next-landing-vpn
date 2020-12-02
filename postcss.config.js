c4a089fef32a1c97ff5dc748ba5c3022a35557fe
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-12-02 11:27:22 +0700

    purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,20 @@
-// const purgecss = [
-//   "@fullhuman/postcss-purgecss",
-//   {
-//     content: [
-//       "./pages/*.js",
-//       "./pages/**/*.js",
-//       "./components/*.js",
-//       "./components/**/*.js",
-//     ],
-//     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-//   },
-// ];
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./pages/*.js",
+      "./pages/**/*.js",
+      "./components/*.js",
+      "./components/**/*.js",
+    ],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
     "autoprefixer",
-    // ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
