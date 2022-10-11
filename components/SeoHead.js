4cab170055d0e6ce57212bbc15409202e8cf7ea5
dianyehezkiel <dianyehezkiel@gmail.com>
2022-10-11 19:16:08 +0700

    update win 8 app icon config, and add some comments

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -1,17 +1,40 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+
+// Default value for some meta data
 const defaultMeta = {
   title: 'LaslesVPN',
   siteName: 'LaslesVPN',
   description:
     'Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest',
+  // change base url of your web (without '/' at the end)
   url: 'https://next-landing-vpn-five.vercel.app',
   type: 'website',
   robots: 'follow, index',
+  // change with url of your image (recommended dimension = 1.91:1)
+  // used in twitter, facebook, etc. card when link copied in tweet/status 
   image: 'https://next-landing-vpn-five.vercel.app/assets/card-image.png',
+  author: 'Lorem Ipsum'
 };
 
+/**
+ * Next Head component populated with necessary SEO tags and title
+ * props field used:
+ * - title
+ * - siteName
+ * - description
+ * - url
+ * - type
+ * - robots
+ * - image
+ * - date
+ * - author
+ * - templateTitle
+ * all field are optional (default value defined on defaultMeta)
+ * @example
+ * <SeoHead title="Page's Title" />
+ */
 const SeoHead = (props) => {
   const router = useRouter();
   const meta = {
@@ -19,6 +42,8 @@ const SeoHead = (props) => {
     ...props
   };
 
+  // Use siteName if there is templateTitle
+  // but show full title if there is none
   meta.title = props.templateTitle
     ? `${props.templateTitle} | ${meta.siteName}`
     : meta.title;
@@ -53,26 +78,27 @@ const SeoHead = (props) => {
           <meta
             name='author'
             property='article:author'
-            content='Theodorus Clarence'
+            content={meta.author}
           />
         </>
       )}
-
       {/* Favicons */}
       {favicons.map((linkProps) => (
         <link key={linkProps.href} {...linkProps} />
       ))}
-      {/* <meta name='msapplication-TileColor' content='#F53838' />
+      {/* Windows 8 app icon */}
+      <meta name='msapplication-TileColor' content='#F53838' />
       <meta
         name='msapplication-TileImage'
         content='/favicon/ms-icon-144x144.png'
-      /> */}
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      />
+      {/* Accent color on supported browser */}
       <meta name='theme-color' content='#F53838' />
     </Head>
   );
 };
 
+// Favicons, other icons, and manifest definition
 const favicons = [
   {
     rel: 'apple-touch-icon',
