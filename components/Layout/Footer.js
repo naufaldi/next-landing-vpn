1bd5c96c4685dec6076c5ea2a7779eee9f40cd19
jayzumen <reinhardtjanniklas96@gmail.com>
2022-11-07 17:44:01 +0100

    added custom Tailwind classes for footer Links

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -5,82 +5,63 @@ import Twitter from "../../public/assets/Icon/twitter.svg";
 import Instagram from "../../public/assets/Icon/instagram.svg";
 const Footer = () => {
   return (
-    <div className="bg-white-300 pt-44 pb-24">
-      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
-        <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          <LogoVPN className="h-8 w-auto mb-6" />
-          <p className="mb-4">
-            <strong className="font-medium">LaslesVPN</strong> is a private
-            virtual network that has unique features and has high security.
+    <div className='bg-white-300 pt-44 pb-24'>
+      <div className='max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4'>
+        <div className='row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start '>
+          <LogoVPN className='h-8 w-auto mb-6' />
+          <p className='mb-4'>
+            <strong className='font-medium'>LaslesVPN</strong> is a
+            private virtual network that has unique features and has
+            high security.
           </p>
-          <div className="flex w-full mt-2 mb-8 -mx-2">
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Facebook className="h-6 w-6" />
+          <div className='flex w-full mt-2 mb-8 -mx-2'>
+            <div className='socialLink'>
+              <Facebook className='h-6 w-6' />
             </div>
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Twitter className="h-6 w-6" />
+            <div className='socialLink'>
+              <Twitter className='h-6 w-6' />
             </div>
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Instagram className="h-6 w-6" />
+            <div className='socialLink'>
+              <Instagram className='h-6 w-6' />
             </div>
           </div>
-          <p className="text-gray-400">©{new Date().getFullYear()} - LaslesVPN</p>
+          <p className='text-gray-400'>
+            ©{new Date().getFullYear()} - LaslesVPN
+          </p>
         </div>
-        <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
-          <ul className="text-black-500 ">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Download{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Pricing{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Locations{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Server{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Countries{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Blog{" "}
-            </li>
+        <div className=' row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col'>
+          <p className='text-black-600 mb-4 font-medium text-lg'>
+            Product
+          </p>
+          <ul className='text-black-500 '>
+            <li className='footerLink'>Download </li>
+            <li className='footerLink'>Pricing </li>
+            <li className='footerLink'>Locations </li>
+            <li className='footerLink'>Server </li>
+            <li className='footerLink'>Countries </li>
+            <li className='footerLink'>Blog </li>
           </ul>
         </div>
-        <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
-          <ul className="text-black-500">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              LaslesVPN ?{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              FAQ{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Tutorials{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              About Us{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Privacy Policy{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Terms of Service{" "}
-            </li>
+        <div className='row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col'>
+          <p className='text-black-600 mb-4 font-medium text-lg'>
+            Engage
+          </p>
+          <ul className='text-black-500'>
+            <li className='footerLink'>LaslesVPN ? </li>
+            <li className='footerLink'>FAQ </li>
+            <li className='footerLink'>Tutorials </li>
+            <li className='footerLink'>About Us </li>
+            <li className='footerLink'>Privacy Policy </li>
+            <li className='footerLink'>Terms of Service </li>
           </ul>
         </div>
-        <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Earn Money</p>
-          <ul className="text-black-500">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Affiliate{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Become Partner{" "}
-            </li>
+        <div className='row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col'>
+          <p className='text-black-600 mb-4 font-medium text-lg'>
+            Earn Money
+          </p>
+          <ul className='text-black-500'>
+            <li className='footerLink'>Affiliate </li>
+            <li className='footerLink'>Become Partner </li>
           </ul>
         </div>
       </div>
